10e2300ae42064e86f7524599e9c4c1e2ef241b8
nazwa-uzytkownika <58075465+nazwa-uzytkownika@users.noreply.github.com>
2019-12-04 14:17:28 +0100

    Update controller.js

--- gry/tetris/js/controller.js
+++ gry/tetris/js/controller.js
@@ -12,11 +12,4 @@ document.body.onkeydown = function( e ) {
     }
 };
 
-bl.onmousedown=function() {leftPressed = true;}
-bl.ontouchstart=function() {leftPressed = true;}
-bl.onmouseup=function() {leftPressed = false;}
-bl.ontouchend=function() {leftPressed = false;}
-bp.onmousedown=function() {rightPressed = true;}
-bp.ontouchstart=function() {rightPressed = true;}
-bp.onmouseup=function() {rightPressed = false;}
-bp.ontouchend=function() {rightPressed = false;}
+
